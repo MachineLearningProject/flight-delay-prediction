76fea68797cad364526b8a79fdd62bf7b9895953
Cesar Rodriguez Villanueva <cesar.rdz.v@gmail.com>
2016-02-14 21:43:44 +0200

    Introduce firebase

--- app/config.py
+++ app/config.py
@@ -6,3 +6,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class BaseConfig(object):
     SECRET_KEY = os.environ.get("SECRET_KEY", "")
     DEBUG = os.environ.get("DEBUG", True)
+    FIREBASE = os.environ.get("FIREBASE", "https://flight-delay-predict.firebaseio.com")
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
 from flask import request, jsonify, session, g
 
 from . import app
+from . import firebase
 
 @app.route("/", methods=["GET"])
 def index():
-    return jsonify({"message" : "hi"})
+    response = firebase.get("/", None)
+    response = response or {}
+    return jsonify(response)
