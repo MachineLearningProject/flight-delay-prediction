b7c499daaae2b0d640202ff7348749800117398c
Cesar Rodriguez Villanueva <cesar.rdz.v@gmail.com>
2016-02-24 14:41:31 +0200

    Add firebase clean

--- app/config.py
+++ app/config.py
@@ -7,6 +7,7 @@ class BaseConfig(object):
     SECRET_KEY = os.environ.get("SECRET_KEY", "")
     DEBUG = os.environ.get("DEBUG", True)
     FIREBASE = os.environ.get("FIREBASE", "https://flight-delay-predict.firebaseio.com")
+    FIREBASE_CLEAN = os.environ.get("FIREBASE", "https://flight-delay-clean.firebaseio.com/")
     FIREBASE_AIRPORT_DATE = os.environ.get("FIREBASE_AIRPORT_DATE", "https://publicdata-airports.firebaseio.com")
     # in seconds
     CRONJOB_CYCLE = os.environ.get("CRONJOB_CYCLE", 3600)
