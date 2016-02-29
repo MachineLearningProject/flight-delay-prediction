80143357184f8b22b7e2bff31eb91dce78189d91
Angel Gallegos <angel.gallegosx@gmail.com>
2016-02-29 23:27:55 +0200

    merge

--- app/predict.py
+++ app/predict.py
@@ -156,125 +156,7 @@ class Predictor:
         wind = [ cleaned_data["wind_x"], cleaned_data["wind_y"] ]
         time_binarized = self.binarize_time(datetime.now())
 
-<<<<<<< Updated upstream
         return np.concatenate((weather_binarized, wind, time_binarized))
-=======
-    firebase_clean.firebase.put("/metadata", name="weathers", data=weather_dict)
-    return weather_dict
-
-def get_weather_array(weather_dict, weather_str):
-    cleaned = clean_weather_string(weather_str)
-    arr = np.zeros(len(weather_dict))
-    if cleaned in weather_dict:
-        pos = weather_dict[ cleaned]
-        arr[pos] = 1
-    else:
-        print "ERROR could not clean:", weather_str
-    return arr
-
-def get_all_weathers_binarized(clean_data):
-    firebase_clean = mapper.get_clean_firebase()
-    weathers = firebase_clean.get_metadata()["weathers"]
-
-    weather_series = []
-    for code, events in clean_data.items():
-        for date, event in events.items():
-            weather_series.append(get_weather_array(weathers, event["weather"]))
-
-    return np.array(weather_series)
-
-def get_all_delays_binarized(clean_data):
-    delay_series = []
-    for code, events in clean_data.items():
-        for date, event in events.items():
-            delay_series.append(event["delay"])
-
-    return np.array(delay_series)
-
-def get_all_airports_binarized(clean_data):
-
-    transformed = DatasetCreation.writeCities_Airports(clean_data)
-    airports_binarized = []
-    for code, events in clean_data.items():
-
-        for date, event in events.items():
-            airports_binarized.append(DatasetCreation.getAirportBinarizedRepresentation(transformed, code))
-
-    return np.array(airports_binarized)
-
-def get_all_wind_binarized(clean_data):
-
-    windPair = []
-    for code, events in clean_data.items():
-        for date, event in events.items():
-            windPair.append( (float(event["wind_x"]), float(event["wind_y"])) )
-
-    return np.array(windPair)
-
-def merge_binarized(arrays):
-    return np.concatenate(arrays, axis=1)
-
-def permute(datapoints, labels):
-
-    p = np.random.permutation(len(labels))
-
-    datapoints = np.array(datapoints)
-    labels = np.array(labels)
-
-    return (datapoints, labels)
-
-def decide_model(datapoints, labels):
-
-    classifiers = []
-    classifiers.append( RandomForestClassifier(n_estimators=10, min_samples_split=1) )
-    classifiers.append( DecisionTreeClassifier(max_depth=None, min_samples_split=1, random_state=0) )
-    classifiers.append( linear_model.Perceptron() )
-    #classifiers.append( linear_model.SGDClassifier() )
-    #classifiers.append( KNeighborsClassifier() )
-
-    best = 0
-    model = None
-    for clf in classifiers:
-        fit = clf.fit(datapoints, labels)
-        scores = cross_val_score(fit, datapoints, labels, cv=10, n_jobs=-1)
-        res = scores.mean()
-
-        if res > best:
-            best = res
-            model = fit
-
-    print "----", model, best
-    return model
-
-def filter_time(clean_data):
-
-    filteredData = clean_data.copy() # hard copy
-    lowLimit = (datetime.now().now() - timedelta(hours=3)).time()
-    highLimit = (datetime.now().now() + timedelta(hours=3)).time()
-
-    for code, events in clean_data.items():
-        for date, event in events.items():
-            parsedDate = parse(date.split()[0]).time()
-            #print lowLimit, parsedDate, highLimit, (parsedDate < highLimit) and (parsedDate > lowLimit)
-     
-            # high above 24 hours
-            if datetime.now().time() > highLimit:
-                
-                if not ( (parsedDate < highLimit) or (parsedDate > lowLimit) ): 
-                    del filteredData[code][date]
-
-            # low below 0 hrs
-            elif  lowLimit > datetime.now().time():
-                
-                if not ( (parsedDate < highLimit) or (parsedDate < lowLimit) ):
-                    del filteredData[code][date]
-            else:
-                
-                if not ( (parsedDate < highLimit) and (parsedDate > lowLimit) ):
-                    del filteredData[code][date]
-            
-    return filteredData
->>>>>>> Stashed changes
 
     def predict(self, airport_code):
         firebase_source = mapper.get_source_firebase()
@@ -283,16 +165,8 @@ def filter_time(clean_data):
         airport_binarized = self.binarize_airport(airport_code, airport_status)
         return self.model.predict([airport_binarized])
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
     def predict_all(self):
         firebase_source = mapper.get_source_firebase()
-=======
-    all_clean = filter_time(all_clean)
->>>>>>> Stashed changes
-=======
-    all_clean = filter_time(all_clean)
->>>>>>> Stashed changes
 
         all_source = firebase_source.get_all()
         results = {}
